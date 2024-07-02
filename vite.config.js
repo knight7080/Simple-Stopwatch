cd0c6f0aa085f143ea07eda2dc0434c34349b9ec
knight7080 <kaushik.code0@gmail.com>
2024-07-02 21:03:58 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://knight7080.github.io/Simple-Stopwatch',
 })
